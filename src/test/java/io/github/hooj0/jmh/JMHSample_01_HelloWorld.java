ed7d43e12a20a73061df9a2972c3ca6ee5e8668a
hoojo <hoojo@qq.com>
2020-12-18 15:35:56 +0800

    :sparkles: :bento: :tada: :wrench: Init add saveactions_settings xml

--- src/test/java/io/github/hooj0/jmh/JMHSample_01_HelloWorld.java
+++ src/test/java/io/github/hooj0/jmh/JMHSample_01_HelloWorld.java
@@ -1,7 +1,31 @@
-package io.github.hooj0.jmh;/**
- * TODO
+package io.github.hooj0.jmh;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+/**
+ * hello world
+ *
  * @author hoojo
- * @date 2022/2/17 0017 10:53
  * @version 1.0
- */public class JMHSample_01_HelloWorld {
+ * @date 2022/2/17 0017 10:53
+ */
+public class JMHSample_01_HelloWorld {
+
+    @Benchmark
+    public void sayHello() {
+        //System.out.println("hello world!");
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(JMHSample_01_HelloWorld.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
 }
